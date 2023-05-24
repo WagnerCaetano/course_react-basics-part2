e19816c8a7d79d60aab63125dd53b4d6173a11a4
wagnercaetano <wagnercaetano01@gmail.com>
2023-05-23 21:48:16 -0300

    setup and init

---
+++ src/components/Posts/index.tsx
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+import "./style.css";
+
+export const Posts = () => {
+  const params = useParams();
+  const id = params.id ? params.id : "0";
+
+  return (
+    <div className="posts">
+      <h1>Posts {`ID: ${id}`}</h1>
+    </div>
+  );
+};
---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Home } from "./components/Home";
+import "./styles/global.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { About } from "./components/About";
+import { Menu } from "./components/Menu";
+import { Posts } from "./components/Posts";
+import { Redirect } from "./components/Redirect";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/posts/:id" element={<Posts />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/redirect" element={<Redirect />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
